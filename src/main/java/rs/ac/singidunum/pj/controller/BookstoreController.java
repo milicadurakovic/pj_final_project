b6a8ef948c561b91823d17e60431f6cbd2a35aed
milicadurakovic <milicadurakovic2003@gmail.com>
2026-06-02 13:20:26 +0200

    Add delete bookstore

--- src/main/java/rs/ac/singidunum/pj/controller/BookstoreController.java
+++ src/main/java/rs/ac/singidunum/pj/controller/BookstoreController.java
@@ -1,12 +1,19 @@
 package rs.ac.singidunum.pj.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 import rs.ac.singidunum.pj.entity.Bookstore;
 import rs.ac.singidunum.pj.repo.BookstoreRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -19,7 +26,14 @@ public class BookstoreController {
 
     @GetMapping
     public List<Bookstore> getBookstore(){
-        return repository.findAll();
+        return repository.findAllByDeletedAtIsNull();
+    }
+    @DeleteMapping(path = "/{id}")
+    public void deleteBookstoreById(@PathVariable Integer id){
+        Bookstore bookstore =repository.findOneByBookstoreIdAndDeletedAtIsNull(id).orElseThrow();
+        bookstore.setDeletedAt(LocalDateTime.now());
+        repository.save(bookstore);
+
     }
 
 }
--- src/main/java/rs/ac/singidunum/pj/repo/BookstoreRepository.java
+++ src/main/java/rs/ac/singidunum/pj/repo/BookstoreRepository.java
@@ -1,10 +1,17 @@
 package rs.ac.singidunum.pj.repo;
 
+import java.util.Optional;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import rs.ac.singidunum.pj.entity.Bookstore;
 
 @Repository
 public interface BookstoreRepository  extends JpaRepository<Bookstore, Integer> {
+
+
+    List<Bookstore> findAllByDeletedAtIsNull();
+    Optional<Bookstore> findOneByBookstoreIdAndDeletedAtIsNull(Integer id);
 
 }
